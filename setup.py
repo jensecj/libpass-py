9cb144f90d00cc84d9517352cc5ef3d53c23b1cf
Jens Christian Jensen <jensecj@gmail.com>
2019-10-25 09:31:00 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup
+
+setup(
+    name="libpass",
+    version="0.1.0",
+    description="Library for accessing zx2c4's PasswordStore.",
+    author="Jens Christian Jensen",
+    author_email="jensecj@gmail.com",
+    url="http://github.com/jensecj/libpass-py",
+    install_requires=[],
+    packages=["libpass"],
+)
