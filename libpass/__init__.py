71e3c9dc945ca8f4aaa31b66c783eab8d22a4e0f
Jens Christian Jensen <jensecj@gmail.com>
2019-10-31 10:04:15 +0100

    minor cleanup, added comments

--- libpass/__init__.py
+++ libpass/__init__.py
@@ -4,28 +4,25 @@ import subprocess
 
 
 class PasswordStore:
-    def __init__(self, path=None):
-        if path:
-            path = os.path.expanduser(path)
+    def __init__(self, store_path=None):
+        # TODO: locate gpg binary, or error
+        # TODO: locate pass binary, or error
 
-            if not os.path.exists(path):
-                raise FileNotFoundError(
-                    f"Path to password store does not exist: {path}"
-                )
-
-            if not os.path.isdir(path):
-                raise ValueError(f"Path to password store is not a directory: {path}")
-
-            self._path = path
+        if store_path:
+            store_path = os.path.expanduser(store_path)
         else:
-            self._path = os.path.expanduser("~/.password-store")
+            store_path = os.path.expanduser("~/.password-store")
 
-        print(self._path)
+        if not os.path.exists(store_path):
+            raise FileNotFoundError(f"Path to password store does not exist: {store_path}")
+
+        if not os.path.isdir(store_path):
+            raise ValueError(f"Path to password store is not a directory: {store_path}")
+
+        self._store_path = store_path
 
     def _cmd(self, cmd):
-        popen = subprocess.Popen(
-            cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True
-        )
+        popen = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
         return_code = popen.wait()
 
@@ -59,11 +56,14 @@ class PasswordStore:
         """
         Return a list of all entries in the password store.
         """
+        # an entry in the password store is the relative path from the
+        # root of the store, to some .gpg file in a subdirectory.
+
         files = []
         for root, _, fs in os.walk(self._path):
             files += [os.path.join(root, f) for f in fs]
 
-        rel_files = [os.path.relpath(f, start=self._path) for f in files]
+        rel_files = [os.path.relpath(f, start=self._store_path) for f in files]
         rel_files.remove(".gpg-id")
 
         entries = [f.strip(".gpg") for f in rel_files]
@@ -75,3 +75,15 @@ class PasswordStore:
         Return the secret for `path', if found in the password store, None otherwise.
         """
         return self._cmd(["pass", path])
+
+    def set(self, path, secret):
+        """
+        Insert `secret' into the password store, at `path'.
+        """
+        self._cmd(["pass", "insert", path, secret])
+
+    def generate(self, path):
+        """
+        Insert a generated secret into `path' in the password store.
+        """
+        self._cmd(["pass", "generate", path])
